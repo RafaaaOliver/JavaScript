ff5642962f51203f15bc443a11e4e34af54d27b7
rafaellstos@hotmail.com <rafaellstos@hotmail.com>
2022-07-11 17:23:42 -0300

    finalizando

--- JavaScript advanced Array, Object and functions/AdvancedFunctions/A37_paramsFunction/index.js
+++ JavaScript advanced Array, Object and functions/AdvancedFunctions/A37_paramsFunction/index.js
@@ -4,7 +4,6 @@
  * Funciona apenas com function normal
  */
 
-
 function funcao() { // funciona apenas com a function normal
     let total = 0;
     for (let argumento of arguments) {
@@ -13,8 +12,6 @@ function funcao() { // funciona apenas com a function normal
     console.log(total)
 }
 funcao(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
-
 /**
  * se for criada uma função e não for passado parâmetros suficientes, o valor será undefined
  */
@@ -23,7 +20,28 @@ function semArgs(a, b, c, d, e, f) {
     console.log(a, b, c, d, e, f)
 }
 semArgs(1, 2, 3)
+/**
+ * se não for passado o parâmetro para a função, ela assumirá o valor padrão.
+ * a única forma de usar o valor padrão é passar undefined como parâmetro
+ */
+function umOuOutro(a, b = 12) {
+    console.log('A: ', a)
+    console.log('B: ', b)
+}
+umOuOutro(25)
 
 /**
- * 
+ * o rest operator cria um array com os parametros 
  */
+
+function conta(operador, acumulador, ...numeros) {
+    for(let numero of numeros) {
+        if (operador === '+') acumulador += numero;
+        if (operador === '-') acumulador -= numero;
+        if (operador === '*') acumulador *= numero;
+        if (operador === '/') acumulador /= numero;
+    }
+    console.log(acumulador)
+}
+
+conta('*', 55, 20, 30, 40, 5000)
